1d7bea2dda6142a4cd2f00fe143562404d77313e
hello@bennyschmidt.com <hello@bennyschmidt.com>
2022-02-25 18:25:59 -0800

    card styles

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const FernSDK = window.FernSDK = {
   package: {
     name: 'fern-sdk',
-    version: '0.0.6'
+    version: '0.0.9'
   },
   Frond: ({
     rootElement,
@@ -68,6 +68,7 @@ const FernSDK = window.FernSDK = {
     const bindCardListEvents = ({ cards }) => {
       const addCardButton = document.getElementById('add-card');
       const saveCardButton = document.getElementById('save-card');
+      const overlay = document.querySelector('#overlay');
 
       const onClickCloseOverlay = () => {
         overlay.removeAttribute('class');
@@ -117,9 +118,9 @@ const FernSDK = window.FernSDK = {
       walletOverlay.onclick = event => {
         if (
           event.target.localName === 'body' ||
-          event.target.className === 'frond-wallet-overlay'
+          event.target.className.match('frond-wallet-overlay')
         ) {
-          walletOverlay.remove();
+          walletOverlay.setAttribute('class', 'frond-wallet-overlay');
         }
       };
 
@@ -130,133 +131,9 @@ const FernSDK = window.FernSDK = {
     };
 
     const onShow = ({ cards }) => {
-      const frondWalletOverlay = document.createElement('div');
-      const frondWalletOverlayStyles = document.createElement('style');
+      const frondWalletOverlay = document.querySelector('.frond-wallet-overlay');
 
-      frondWalletOverlay.setAttribute('class', 'frond-wallet-overlay');
-      frondWalletOverlayStyles.setAttribute('scoped', true);
-
-      frondWalletOverlayStyles.innerHTML = `
-        .frond-wallet-overlay {
-          position: fixed;
-          background: rgba(0, 0, 0, .8);
-          left: 220px;
-          right: 0;
-          top: 0;
-          bottom: 0;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: column;
-          z-index: 800;
-        }
-        .frond-wallet {
-          display: flex;
-          flex-direction: column;
-          background: #111;
-          color: white;
-          box-shadow: 0 0 1rem black;
-          border-radius: 0.5rem;
-          border: 1px solid black;
-          padding: 1rem;
-          margin: 2rem;
-          overflow: auto;
-        }
-        .frond-wallet > h3 {
-          color: white;
-        }
-        .card-list {
-          display: flex;
-          flex-wrap: wrap;
-          align-items: center;
-          justify-content: flex-start;
-          list-style: none;
-          margin: 0;
-          padding: 0;
-        }
-        .card {
-          display: flex;
-          flex-direction: column;
-          align-items: flex-end;
-          justify-content: center;
-          width: 19rem;
-          height: 12rem;
-          background: linear-gradient(to bottom, #00ffff, #2196f3);
-          margin: 1rem;
-          padding: 2rem;
-          border-radius: 1rem;
-          overflow: hidden;
-        }
-        .card.drv {
-          background: linear-gradient(to bottom, #111, #000);
-        }
-        .card:not(.new) {
-          box-shadow: inset 0 1px rgb(255 255 255 / 50%), 0 2px 1rem rgb(0 10 100);
-        }
-        .card > .number,
-        .card > .details {
-          font-weight: 400;
-          font-family: monospace;
-          text-shadow: 0 1px #1e69b3;
-        }
-        .card > .number {
-          flex: 1;
-          align-self: flex-start;
-        }
-        .card > .details {
-          flex: 1;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          justify-content: space-between;
-          width: 100%;
-          max-height: 36px;
-        }
-        .card.new {
-          display: block;
-          background: transparent;
-          border: 1px dashed white;
-          line-height: 12rem;
-          text-align: center;
-          padding: 0;
-          opacity: .5;
-        }
-        .card.new > button {
-          margin: 0;
-          width: 100%;
-          height: 100%;
-          appearance: none;
-          border: none;
-          background: transparent;
-          color: white;
-        }
-        .card p {
-          margin: 0;
-        }
-        @media (max-width: 900px) {
-          .frond-wallet-overlay {
-            left: 0;
-          }
-          .card-list {
-            display: block;
-          }
-          .card {
-            margin: 2rem auto;
-          }
-          #add-card-overlay {
-            margin: 1rem;
-          }
-        }
-      `;
-
-      frondWalletOverlay.innerHTML = `
-        <div class="frond-wallet">
-          <h3>Payment Method</h3>
-        </div>
-      `;
-
-      rootElement.insertBefore(frondWalletOverlayStyles, rootElement.firstElementChild);
-      rootElement.insertBefore(frondWalletOverlay, frondWalletOverlayStyles.nextElementSibling);
+      frondWalletOverlay.setAttribute('class', 'frond-wallet-overlay show');
 
       if (!renderCardList({
         cards,
@@ -269,6 +146,206 @@ const FernSDK = window.FernSDK = {
         return;
       }
     };
+
+    const frondWalletOverlay = document.createElement('div');
+    const frondWalletOverlayStyles = document.createElement('style');
+
+    frondWalletOverlay.setAttribute('class', 'frond-wallet-overlay');
+    frondWalletOverlayStyles.setAttribute('scoped', true);
+
+    frondWalletOverlay.innerHTML = `
+      <div class="frond-wallet">
+        <h3>Payment Method</h3>
+      </div>
+    `;
+
+    frondWalletOverlayStyles.innerHTML = `
+      .frond-wallet-overlay {
+        display: none;
+        position: fixed;
+        background: rgba(0, 0, 0, .8);
+        left: 220px;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        z-index: 800;
+      }
+      .frond-wallet-overlay.show {
+        display: flex;
+      }
+      .frond-wallet {
+        display: flex;
+        flex-direction: column;
+        background: #111;
+        color: white;
+        box-shadow: 0 0 1rem black;
+        border-radius: 0.5rem;
+        border: 1px solid black;
+        padding: 1rem;
+        margin: 2rem;
+        overflow: auto;
+      }
+      .frond-wallet > h3 {
+        color: white;
+      }
+      .card-list {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: flex-start;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+      }
+      .card {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        justify-content: center;
+        width: 15rem;
+        height: 10rem;
+        background: linear-gradient(to bottom, #00ffff, #2196f3);
+        margin: 1rem;
+        padding: 1rem;
+        border-radius: 1rem;
+        overflow: hidden;
+      }
+      .card.drv {
+        background: linear-gradient(to bottom, #111, #000);
+      }
+      .card:not(.new) {
+        box-shadow: inset 0 1px rgb(255 255 255 / 50%), 0 2px 1rem rgb(0 10 100);
+      }
+      .card > .number,
+      .card > .details {
+        font-weight: 400;
+        font-family: monospace;
+        text-shadow: 0 1px #1e69b3;
+      }
+      .card > .number {
+        flex: 1;
+        align-self: flex-start;
+      }
+      .card > .details {
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        max-height: 36px;
+      }
+      .card.new {
+        display: block;
+        background: transparent;
+        border: 1px dashed white;
+        line-height: 12rem;
+        text-align: center;
+        padding: 0;
+        opacity: .5;
+      }
+      .card.new > button {
+        margin: 0;
+        width: 100%;
+        height: 100%;
+        appearance: none;
+        border: none;
+        background: transparent;
+        color: white;
+      }
+      .card p {
+        margin: 0;
+      }
+      #overlay {
+        display: none;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0, 0, 0, 0.75);
+        align-items: center;
+        justify-content: center;
+        z-index: 850;
+      }
+      #overlay.show {
+        display: flex;
+      }
+      #add-card-overlay {
+        flex: 1;
+        flex-direction: row;
+        flex-wrap: wrap;
+        max-width: 30rem;
+        height: 19rem;
+        align-items: center;
+        justify-content: space-between;
+        margin-left: 220px;
+        padding: 1rem;
+        border: 1px solid black;
+        border-radius: 1rem;
+      }
+      #add-card-overlay > input,
+      #save-card {
+        background: rgba(255, 255, 255, .5);
+        color: #2196f3;
+        border: none;
+        border-radius: 0.5rem;
+        box-shadow: inset 0 1px 0 rgb(255 255 255 / 25%);
+        width: 100%;
+        margin: 0;
+        padding: 1rem;
+        font-family: monospace;
+        font-size: .75em;
+        font-weight: 600;
+      }
+      #add-card-overlay > input::placeholder {
+        color: #2196f3;
+      }
+      #add-card-overlay > input:nth-of-type(3),
+      #add-card-overlay > input:nth-of-type(4) {
+        max-width: 25%;
+      }
+      #save-card {
+        background: white;
+        color: #2196f3;
+        font-weight: 600;
+      }
+      @media (max-width: 900px) {
+        .frond-wallet-overlay {
+          left: 0;
+        }
+        .card-list {
+          display: block;
+        }
+        .card {
+          margin: 2rem auto;
+        }
+        #add-card-overlay {
+          margin: 1rem;
+        }
+      }
+    `;
+
+    const overlay = document.createElement('aside');
+
+    overlay.setAttribute('id', 'overlay');
+
+    overlay.innerHTML = `
+      <form id="add-card-overlay" class="card" action="">
+        <input id="card-number" type="number" placeholder="Card number" required />
+        <input id="card-name" type="text" placeholder="Name on card" required />
+        <input id="card-expiration" type="number" placeholder="Exp." required />
+        <input id="card-cvv" type="number" placeholder="CVV" required />
+        <button id="save-card">Save</button>
+      </form>
+    `;
+
+    rootElement.insertBefore(frondWalletOverlay, frondWalletOverlayStyles.nextElementSibling);
+    rootElement.insertBefore(frondWalletOverlayStyles, rootElement.firstElementChild);
+    rootElement.insertBefore(overlay, notification.nextElementSibling);
 
     return {
       onShow
